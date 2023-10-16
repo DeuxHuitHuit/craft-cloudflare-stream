a57c05a5922a231fcf2eeecbece3439b811511e2
Nicolas Brassard <nitriques@users.noreply.github.com>
2023-10-16 12:44:45 -0400

    (feat) Add reupload cli command
    This allows us to reupload everything when the account info changes.

---
+++ src/controllers/ReuploadController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace deuxhuithuit\cfstream\controllers;
+
+use craft\elements\Asset;
+use yii\console\ExitCode;
+use craft\helpers\Console;
+use craft\console\Controller;
+use deuxhuithuit\cfstream\jobs\UploadVideoJob;
+use deuxhuithuit\cfstream\fields\CloudflareVideoStreamField;
+
+class ReuploadController extends Controller {
+    public $defaultAction = 'reupload';
+
+    public function actionReupload(): int
+    {
+        $fields = \Craft::$app->getFields()->getFieldsByType(CloudflareVideoStreamField::class);
+        $fieldsCount = is_array($fields) ? count($fields) : 0;
+
+        $this->stdout("Found $fieldsCount fields", Console::FG_GREEN);
+        $this->stdout(PHP_EOL);
+
+        $volumes = \Craft::$app->getVolumes()->getAllVolumes();
+        $volumeCount = is_array($volumes) ? count($volumes) : 0;
+
+        $this->stdout("Found $volumeCount volumes", Console::FG_GREEN);
+        $this->stdout(PHP_EOL);
+
+        $uploadCount = 0;
+        foreach ($volumes as $volume) {
+            $this->stdout("Volume: $volume->name");
+            $this->stdout(PHP_EOL);
+            foreach ($fields as $field) {
+                $this->stdout("  Field: $field->handle");
+                $this->stdout(PHP_EOL);
+
+                $entries = Asset::find()->volumeId($volume->id)->all();
+                foreach ($entries as $asset) {
+                    if (isset($asset->videoStream)) {
+                         $uploadJob = new UploadVideoJob([
+                            'fieldHandle' => $field->handle,
+                            'elementId' => $asset->id,
+                            'videoUrl' => $asset->getUrl(),
+                            'videoName' => $asset->title ?? $asset->filename,
+                        ]);
+                        \Craft::$app->getQueue()->push($uploadJob);
+                        $uploadCount++;
+                    }
+                }
+            }
+        }
+
+        $this->stdout("Created $uploadCount new upload jobs!", Console::FG_GREEN);
+        $this->stdout(PHP_EOL);
+
+        return ExitCode::OK;
+    }
+}
