c1b6e844ffc3cf7eb13d4561b23b4826def009ca
Nicolas Brassard <nitriques@users.noreply.github.com>
2024-10-09 16:01:58 -0400

    (fix) Use `path` property instead of `rootPath`
    Port of #13 / d52d1ecdf8 to v1
    
    Turns out latest craft 4 requires it.

--- src/Folder.php
+++ src/Folder.php
@@ -2,25 +2,36 @@
 
 namespace deuxhuithuit\cfstream;
 
+use craft\elements\Asset;
+use craft\helpers\App;
+
 class Folder
 {
     /**
-     * Returns the full path of the asset's folder
-     * @param \craft\elements\Asset $asset
+     * Returns the full path of the asset's folder.
+     *
+     * @param Asset $asset
      */
     public static function getAssetFolderPath($asset): string
     {
         $fs = $asset->getVolume()->getFs();
         $path = '';
 
-        if (property_exists($fs, 'rootPath')) {
+        // Start with the volume's path
+        if ($fs->path) {
+            $path = App::parseEnv($fs->path);
+        }
+        // or start with rootPath, if it exists?
+        elseif (property_exists($fs, 'rootPath')) {
             $path = $fs->rootPath;
         }
 
+        // Add the asset's folder path
         $folderPath = $asset->getFolder()->path;
         if ($folderPath) {
             $path .= '/' . $folderPath;
         }
+
         return $path;
     }
 }
