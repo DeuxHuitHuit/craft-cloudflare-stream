392501547ab84b3e54ae6d3a7c65be86f301b972
Patrick Long <78626999+patrick-long@users.noreply.github.com>
2026-02-19 12:23:55 -0600

    (feat) Add duration field in GraphQL CloudflareVideoStreamType (#20)
    (fix) Proper type
    
    Author:    Patrick Long <78626999+patrick-long@users.noreply.github.com>
    Date:      Thu Feb 19 12:23:55 2026 -0600

--- src/graphql/types/CloudflareVideoStreamType.php
+++ src/graphql/types/CloudflareVideoStreamType.php
@@ -74,6 +74,18 @@ class CloudflareVideoStreamType extends ObjectType
                         return strval($value['mp4Url']);
                     },
                 ],
+                'duration' => [
+                    'name' => 'duration',
+                    'type' => Type::int(),
+                    'description' => 'The duration of the video.',
+                    'resolve' => function ($value) {
+                        if (!isset($value['duration'])) {
+                            return null;
+                        }
+
+                        return intval($value['duration']);
+                    },
+                ],
                 'ready' => [
                     'name' => 'ready',
                     'type' => Type::boolean(),
